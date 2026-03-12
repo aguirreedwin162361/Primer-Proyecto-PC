789840aa9b5498e7770e8e072fe9de13cc815733
aguirreedwin162361 <aguirreedwin162361@gmail.com>
2026-03-12 17:17:09 -0600

    Primer commit

---
+++ proyecto.cs
@@ -0,0 +1,32 @@
+int opcion;
+string contenido = "";
+int duracion;
+string clasificacion = "";
+int hora_programada;
+string produccion = "";
+int totalEvaluados = 0;
+int publicados = 0;
+int rechazados = 0;
+int revision = 0;
+
+do
+{
+    Console.WriteLine("MENÚ");
+    Console.WriteLine("1.Evaluar nuevo contenido");
+    Console.WriteLine("2.Mostrar Reglas del Sistema");
+    Console.WriteLine("3.Mostrar estadísticas de la sesión");
+    Console.WriteLine("4.Reiniciar estadísticas");
+    Console.WriteLine("5.Salir");
+
+     Console.Write("Seleccione una opción: ");
+     opcion = int.Parse(Console.ReadLine());
+
+     switch (opcion)
+     {
+        case 1:
+        Console.Write("Ingrese tipo de contenido (película, serie, documental, evento en vivo): ");
+        contenido = Console.ReadLine();
+
+     }
+
+} while (opcion !=5);
